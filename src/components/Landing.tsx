841ea5a7f16501531a37f36c9a420e1158d03273
Guido Rann <guidorann.code@gmail.com>
2024-02-19 22:07:39 -0300

    cambio del boton por link para redireccion

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
@@ -6,14 +7,12 @@ export default function Landing() {
     <div className="flex flex-col items-center h-screen text-center text-white bg-[url('/src/images/fondo/magic.png')] bg-cover">
       <Navbar />
       <div className="flex flex-col items-center gap-5 justify-evenly h-screen w-screen">
-        <button
+        <Link
+          to="/board"
           className="flex justify-center bg-blue-600 text-white gap-1 px-8 py-3 cursor-pointer font-semibold tracking-widest rounded-md hover:bg-blue-400 duration-300 hover:gap-2 hover:translate-x-3"
-          onClick={() => {
-            window.location.href = "/board";
-          }}
         >
           JUGAR
-        </button>
+        </Link>
       </div>
       <Footer />
     </div>

22107f6c846d78a027ce15ba93dc89a6bc66cb29
Guido Rann <guidorann.code@gmail.com>
2024-02-29 15:11:59 -0300

    Redesign modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,19 +7,17 @@ export default function Modal({ isOpen, onClose }: modalProps) {
   return (
     <>
       {isOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
-          <section className="bg-[#333]  h-[200px] w-[300px] rounded">
-            <div className="flex justify-end ">
-              <button
-                className="flex justify-center items-center cursor-pointer text-blue-300 hover:text-blue-500 duration-100 bg-slate-700 hover:bg-slate-600 w-6 h-6 m-1 rounded-sm"
-                onClick={onClose}
-              >
-                x
-              </button>
-            </div>
-            <div className="h-3/4 flex justify-center items-center text-blue-300 font-poppins">
+        <div className="animate-fade-in animate-duration-300 fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
+          <section className="bg-[#045860] h-[200px] w-[300px] flex flex-col justify-center items-center rounded-2xl">
+            <div className="w-full flex justify-center items-center h-1/2 text-white font-poppins text-xl font-bold">
               <h1>¡Felicitaciones GANASTE!</h1>
             </div>
+            <button
+              className="flex justify-center bg-teal-600 text-white px-2 py-2 md:px-7 md:py-3 cursor-pointer font-semibold rounded-md hover:scale-110 duration-300"
+              onClick={onClose}
+            >
+              Volver a jugar
+            </button>
           </section>
         </div>
       )}

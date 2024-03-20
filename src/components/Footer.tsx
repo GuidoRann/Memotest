6b03c452a9db51d6760bd3ecdeb49f82f3bf67ff
Guido Rann <guidorann.code@gmail.com>
2024-03-20 17:58:40 -0300

    Adding details

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div>
       <p className="text-white bg-slate-800 opacity-80 w-screen h-[40px] flex items-center justify-center">
-        &lt;&gt; con &#128151; por GuidoCode
+        &lt;&gt; con ❤️ por GuidoCode
       </p>
     </div>
   );
